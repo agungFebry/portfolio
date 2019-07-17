26c91d4ad9a364a4dcde2519daf43808d9ac50e2
agungfebry <49956789+agungFebry@users.noreply.github.com>
2019-07-17 23:29:52 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,19 @@ $(document).ready(function()
             scrollTop: $(hash).offset().top - 50
             }, 1000);
         });
+    
+    $(window).scroll(function()
+        {
+        var y = $(window).scrollTop();
+
+        if(y >= 585)
+            {  
+            $('#navbarTest').slideDown(500).css('background-image','linear-gradient(45deg, #0066ff, #9900ff)');
+            }
+        else
+            {
+            $('#navbarTest').css('background-image','none')
+            }
+        });
+
     });
